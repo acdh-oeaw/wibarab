89eac8310556018659177c89aa30aaa35437e992
Stefan Probst <stefanprobst@kolabnow.com>
2021-11-06 23:22:36 +0100

    chore: disable google translate

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -3,7 +3,7 @@ import NextDocument, { Head, Html, Main, NextScript } from 'next/document'
 export default class Document extends NextDocument {
   render(): JSX.Element {
     return (
-      <Html lang="en">
+      <Html lang="en" translate="no">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
