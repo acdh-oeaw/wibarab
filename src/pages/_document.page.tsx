8dd4a9c17f93274065125bc6b50e603876a05a82
Stefan Probst <stefanprobst@kolabnow.com>
2021-11-11 14:39:13 +0100

    refactor: use functional document

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -1,22 +1,22 @@
-import NextDocument, { Head, Html, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document'
 
-export default class Document extends NextDocument {
-  render(): JSX.Element {
-    return (
-      <Html lang="en" translate="no">
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:ital,wght@0,400..700;1,400..700&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+/* eslint-disable @next/next/no-page-custom-font */
+
+export default function Document(): JSX.Element {
+  return (
+    <Html lang="en" translate="no">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:ital,wght@0,400..700;1,400..700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
