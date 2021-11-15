de7202a156d290b621d101c049da4ec80168d22e
Stefan Probst <stefanprobst@posteo.de>
2021-11-15 10:33:29 +0100

    fix: use updated page extension

--- src/lib/data/data.config.ts
+++ src/lib/data/data.config.ts
@@ -4,4 +4,4 @@ export const teamFolderPath = path.join(process.cwd(), 'src', 'components', 'hom
 export const teamExtension = '.json'
 
 export const blogFolderPath = path.join(process.cwd(), 'src', 'pages', 'blog')
-export const articleExtension = '.mdx'
+export const articleExtension = '.page.mdx'
