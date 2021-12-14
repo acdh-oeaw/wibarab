58e1c731fc67c072c5ec97c8f70293c33bcf4e07
Stefan Probst <stefanprobst@posteo.de>
2021-12-14 21:13:19 +0100

    feat: add image optimisation

--- src/components/blog/ArticlePageLayout.tsx
+++ src/components/blog/ArticlePageLayout.tsx
@@ -1,39 +1,58 @@
 import { SchemaOrg } from '@stefanprobst/next-page-metadata'
+import cx from 'clsx'
 import type { ReactNode } from 'react'
 import { Fragment } from 'react'
 
 import { ArticleHeaderSection } from '@/components/blog/ArticleHeaderSection'
 import { ArticleMedata } from '@/components/blog/ArticleMetadata'
+import { Image } from '@/components/Image'
 import { PageHeroLeadIn } from '@/components/PageHeroLeadIn'
 import { PageHeroTitle } from '@/components/PageHeroTitle'
 import { PageMainContent } from '@/components/PageMainContent'
 import { PageSection } from '@/components/PageSection'
 import { PageMetadata } from '@/lib/core/metadata/PageMetadata'
 import type { ArticleMetadata } from '@/lib/data/types'
-import { isNonEmptyString } from '@/lib/utils'
 
 export interface ArticlePageLayoutProps {
   children?: ReactNode
   metadata: ArticleMetadata
+  isPreview?: boolean
 }
 
 export function ArticlePageLayout(props: ArticlePageLayoutProps): JSX.Element {
   const { title, leadIn, date, authors, featuredImage, abstract } = props.metadata
 
-  const hasFeaturedImage = isNonEmptyString(featuredImage)
-  const ogImage = hasFeaturedImage ? { images: [{ src: featuredImage, alt: '' }] } : {}
-  const schemaOrgImage = hasFeaturedImage ? { image: featuredImage } : {}
+  const ogImages =
+    featuredImage != null
+      ? typeof featuredImage === 'object'
+        ? { images: [{ ...featuredImage, alt: '' }] }
+        : { images: [{ src: featuredImage, alt: '' }] }
+      : {}
+
+  const schemaOrgImageObject =
+    featuredImage != null
+      ? typeof featuredImage === 'object'
+        ? {
+            image: {
+              '@type': 'ImageObject' as const,
+              contentUrl: featuredImage.src,
+              width: featuredImage.width + 'px',
+              height: featuredImage.height + 'px',
+            },
+          }
+        : { image: featuredImage }
+      : {}
 
   return (
     <Fragment>
-      <PageMetadata title={title} description={abstract} openGraph={{ ...ogImage }} />
+      <PageMetadata title={title} description={abstract} openGraph={{ ...ogImages }} />
       <SchemaOrg
         schema={{
           '@type': 'Article',
           headline: title,
           abstract: abstract,
           datePublished: date,
-          ...schemaOrgImage,
+          ...schemaOrgImageObject,
           author: authors
             .map((author) => {
               return author.name
@@ -48,13 +67,28 @@ export function ArticlePageLayout(props: ArticlePageLayoutProps): JSX.Element {
           <ArticleMedata metadata={props.metadata} />
         </ArticleHeaderSection>
         <PageSection>
-          {hasFeaturedImage ? (
+          {featuredImage != null ? (
             <div className="mb-6 bleed">
-              <img
-                src={featuredImage}
-                alt=""
-                className="object-cover w-full rounded aspect-[16/10]"
-              />
+              {typeof featuredImage === 'object' ? (
+                <Image
+                  priority
+                  src={featuredImage}
+                  alt=""
+                  objectFit="cover"
+                  className="w-full rounded"
+                />
+              ) : (
+                /* For external image urls we cannot know image dimensions. */
+                <img
+                  src={featuredImage}
+                  alt=""
+                  className={cx(
+                    'object-cover w-full rounded',
+                    props.isPreview !== true && 'aspect-[16/10]',
+                  )}
+                  loading="lazy"
+                />
+              )}
             </div>
           ) : null}
           <div className="prose">{props.children}</div>
