eb01be67316f8dda43a55f5aa2fef716c2e7bb86
Angelika Hechtl <angelika.hechtl@gmail.com>
2022-01-03 15:48:02 +0100

    update final version

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -33,30 +33,15 @@ export default function HomePage(_props: HomePage.Props): JSX.Element {
         </PageHeroSection>
         <PageSection>
           <div className="prose">
+
             <p>
-              More than 350 million people speak Arabic in linguistic settings that are for the most
-              part characterised by a high degree of diglossia. From Iran to Mauretania, countless
-              spoken varieties are in use, which will be investigated in the ERC project WIBARAB. A
-              particular concern of the project is the language of the Bedouins which has spread
-              with the Arab expansion in the Middle East and North Africa since the 7th century. In
-              a cooperation between the Institute of Oriental Studies (University of Vienna) and the
-              Austrian Centre for Digital Humanities and Cultural Heritage (Austrian Academy of
-              Sciences), an attempt is being made to better understand the nature of the linguistic
-              dichotomy between sedentary and nomadic varieties and this concept, which is so
-              fundamental to Arabic language history, by applying innovative and interdisciplinary
-              methods. The project will collect new data through fieldwork in Saudi Arabia, Jordan,
-              Sudan and Morocco, among other countries. It will also include social parameters in
-              the investigation, in particular the question of whether the prevalence of tribal
-              structures and patriarchal social patterns helps to explain the linguistic
-              conservatism of Bedouin dialects.
+              More than 350 million people speak Arabic in linguistic settings that are for the most part characterised by a high degree of diglossia. Some of the countless spoken Arabic varieties in use from Iran to Mauretania will be investigated in the framework of an ERC Advanced Grant by the WIBARAB project (What Is Bedouin-type ARABic?). 
             </p>
             <p>
-              As part of the project, the ACDH-CH will further develop and refine its
-              text-technological stack and take care of data modelling, corpus design, the
-              implementation of the database, the publication platform and long-term preservation.
-              The database will allow for efficient cross-dialectal comparisons, particularly with
-              regard to phonological, morphological, syntactical, phraseological and lexical
-              features
+              As its name indicates, a particular concern of the WIBARAB project is the language of the Bedouins which has spread with the Arab expansion in the Middle East and North Africa since the 7th century. In a cooperation between the Institute of Oriental Studies (University of Vienna) and the Austrian Centre for Digital Humanities and Cultural Heritage (Austrian Academy of Sciences), WIBARAB aims to better understand the nature of the linguistic dichotomy between sedentary and nomadic varieties, a fundamental concept to Arabic language history, by applying innovative and interdisciplinary research methods. To pursue this goal, the project will collect new data through extensive fieldwork in Saudi Arabia, Kuwait, Jordan, Lebanon, Egypt, Tunisia and Morocco. Along with linguistic data, WIBARAB will also investigate social parameters, focusing in particular on the question of whether the prevalence of tribal structures and patriarchal social patterns helps to explain the linguistic conservatism of Bedouin dialects. 
+            </p>
+            <p>
+              Both newly collected and already published data will be systematically stored in a database that will allow for efficient cross-dialectal comparisons, particularly with regard to phonological, morphological, syntactical, phraseological and lexical features. This will be possible thanks to the help of the ACDH-CH team, which will further develop and refine its text-technological stack and take care of data modelling and corpus design, as well as of the implementation, publication and long-term preservation of the project’s database. 
             </p>
           </div>
         </PageSection>
